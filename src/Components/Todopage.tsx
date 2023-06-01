817632a80cf8ab2649593c731453fc5df784af3d
Sabari <sabarihiswaran555@gmail.com>
2023-06-01 22:21:18 +0530

    added input box

--- src/Components/Todopage.tsx
+++ src/Components/Todopage.tsx
@@ -1,19 +1,27 @@
-import React from "react";
 
 import todogiphy from "../Gif/giphy (4).gif";
+import UssersList from "./UssersList";
 
 const Todopage = () => {
+
   return (
+
     <div className="todo-mainpage">
+
       <div className="header-div">
+
         <div>
-          <p className="todo-quote"> Capture Everything in the Todo-lists and make it actionable...</p>
+          <p className="todo-quote"> Capture Everything in the Todo-lists and make it actionable...☑️</p>
         </div>
 
         <div className="todo-gif-div">
           <img src={todogiphy} alt="gif" className="todo-gif-img" />
         </div>
+
       </div>
+
+      <UssersList/>
+
     </div>
   );
 };
---
+++ src/Components/UssersList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UssersList = () => {
+  return (
+    <div className='users-input'>
+        <input type="text" name='usersToDo' className='todo-inputbox'/>
+    </div>
+  )
+}
+
+export default UssersList
