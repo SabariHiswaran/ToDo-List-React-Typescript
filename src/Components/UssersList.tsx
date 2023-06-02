73a7a9345beaf12baae3c24c48109ea45d0a0fd9
Sabari <sabarihiswaran555@gmail.com>
2023-06-02 12:13:18 +0530

    captured input and saved in todo list array

--- src/Components/UssersList.tsx
+++ src/Components/UssersList.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef,useState } from 'react'
 
 const UssersList = () => {
+
+  const [userInput , setUserInput] = useState<string>("")
+
+  const [allList, setAllList] = useState<Array<Object>>([])
+
+  const inputref = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+
+    inputref.current?.focus()
+
+  }, [])
+
+  const handleChange = (e : React.SyntheticEvent) => {
+
+    setUserInput((e.target as HTMLInputElement).value)
+
+  }
+
+  const handleClick = () => {
+      if(userInput.length === 0 ){
+        alert("Please Enter your Task")
+      }
+      else{
+        setAllList(prevVal => [...prevVal , {id : allList.length+1 , todo : userInput , isDone : false}])
+        setUserInput("")
+      }
+      }
+
   return (
+    <>
     <div className='users-input'>
 
         <input 
@@ -9,11 +39,24 @@ const UssersList = () => {
         name='usersToDo' 
         className='todo-inputbox'
         placeholder='Add new Task'
+        ref={inputref}
+        value={userInput}
+        onChange={(e)=>handleChange(e)}
+        required
         />
 
-        <button className='todolist-addbutton'> + </button>
+        <button 
+        className='todolist-addbutton'
+        onClick={handleClick}
+        > 
+        + 
+        </button>
 
     </div>
+
+    
+
+    </>
   )
 }
 
