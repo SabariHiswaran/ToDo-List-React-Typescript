7e3b74157642f5c2698279df66cd75a746ee7731
Sabari <sabarihiswaran555@gmail.com>
2023-06-06 17:41:04 +0530

    starting articles section

--- src/Components/UssersList.tsx
+++ src/Components/UssersList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import List from "./List";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 type allListType = {
   id: string;
   todo: string;
   isDone: boolean;
-  isChecked : boolean
+  isChecked: boolean;
 };
 
 const UssersList = () => {
@@ -14,7 +14,7 @@ const UssersList = () => {
 
   const [allList, setAllList] = useState<Array<allListType>>([]);
 
-  const [allCheckedList, setAllCheckedList] = useState<string[]>([])
+  const [allCheckedList, setAllCheckedList] = useState<string[]>([]);
 
   const isDisabled: boolean = userInput.length > 0 ? false : true;
 
@@ -23,13 +23,11 @@ const UssersList = () => {
   const inputref = useRef<HTMLInputElement>(null);
 
   const unique_id = uuid();
-  const small_id = unique_id.slice(0,8)
+  const small_id = unique_id.slice(0, 8);
 
   useEffect(() => {
     inputref.current?.focus();
-
   }, []);
-
 
   const handleChange = (e: React.SyntheticEvent) => {
     setUserInput((e.target as HTMLInputElement).value);
@@ -38,44 +36,43 @@ const UssersList = () => {
   const handleClick = () => {
     setAllList((prevVal) => [
       ...prevVal,
-      { id:small_id, todo: userInput, isDone: false,isChecked : false },
+      { id: small_id, todo: userInput, isDone: false, isChecked: false },
     ]);
     setUserInput("");
   };
 
-  const handleIsChecked = (isCheckedId : string , statusUpdate : boolean) => {
+  const handleIsChecked = (isCheckedId: string, statusUpdate: boolean) => {
     setAllList((prevVal) => {
       const newArr = prevVal.map((val) => {
         if (val.id === isCheckedId) {
-          return { ...val, isChecked: statusUpdate};
+          return { ...val, isChecked: statusUpdate };
         } else {
           return val;
         }
       });
       return newArr;
     });
+  };
 
-  }
-
-    const handleCheckedList = (checkedStatus : string, checkedId : string ) => {
-
-        if(checkedStatus === "add"){
-          setAllCheckedList(prevVal => [...prevVal ,checkedId ])
-        }
-        else{
-          if(checkedStatus === "remove"){
-            setAllCheckedList(prevVal => prevVal.filter(val => val !== checkedId))
-          }
-        }
-
+  const handleCheckedList = (checkedStatus: string, checkedId: string) => {
+    if (checkedStatus === "add") {
+      setAllCheckedList((prevVal) => [...prevVal, checkedId]);
+    } else {
+      if (checkedStatus === "remove") {
+        setAllCheckedList((prevVal) =>
+          prevVal.filter((val) => val !== checkedId)
+        );
+      }
     }
+  };
 
   const handleRemoveAll = () => {
+    setAllList((prevVal) =>
+      prevVal.filter((val) => !allCheckedList.includes(val.id))
+    );
 
-      setAllList(prevVal => prevVal.filter(val => !allCheckedList.includes(val.id) ))
-
-      setAllCheckedList([])
-  }
+    setAllCheckedList([]);
+  };
 
   const handleTick = (selectedId: string) => {
     setAllList((prevVal) => {
@@ -90,23 +87,21 @@ const UssersList = () => {
     });
   };
 
-  const handleEditedList = (editedId:string, editedListName:string) => {
+  const handleEditedList = (editedId: string, editedListName: string) => {
     setAllList((prevVal) => {
       const newArr = prevVal.map((val) => {
         if (val.id === editedId) {
-          return { ...val, todo: editedListName};
+          return { ...val, todo: editedListName };
         } else {
           return val;
         }
       });
       return newArr;
     });
-  }
+  };
 
   const handleDelete = (deleteId: string) => {
-
-    setAllList((prevVal) => prevVal.filter( (val) => val.id !== deleteId ))
-
+    setAllList((prevVal) => prevVal.filter((val) => val.id !== deleteId));
   };
 
   return (
@@ -131,37 +126,57 @@ const UssersList = () => {
           +
         </button>
       </div>
+      <div>
+        {allList.length > 0 ? (
+          <div className="allTodoList-div">
+            {allList.map((list) => (
+              <List
+                todolist={list}
+                handleTick={handleTick}
+                handleDelete={handleDelete}
+                handleEditedList={handleEditedList}
+                handleCheckedList={handleCheckedList}
+                handleIsChecked={handleIsChecked}
+                key={list.id}
+              />
+            ))}
 
-      {allList.length > 0 ? (
-        <div className="allTodoList-div">
-        
-          {allList.map((list) => (
-            <List 
-            todolist={list} 
-            handleTick={handleTick} 
-            handleDelete={handleDelete} 
-            handleEditedList={handleEditedList}
-            handleCheckedList = {handleCheckedList}
-            handleIsChecked = {handleIsChecked}
-            key={list.id}
-            />
-          ))}
-   
-        <button 
-        className={allCheckedList.length > 1 ? "removeAll-button" : "removeAll-disabledbutton" }
-        onClick={handleRemoveAll}
-        disabled= {allCheckedList.length > 1 ? false : true}
-        >
-          Delete Selected
-          </button>
+            <button
+              className={
+                allCheckedList.length > 1
+                  ? "removeAll-button"
+                  : "removeAll-disabledbutton"
+              }
+              onClick={handleRemoveAll}
+              disabled={allCheckedList.length > 1 ? false : true}
+            >
+              Delete Selected
+            </button>
+          </div>
+        ) : (
+          <div className="notask-div">
+            <p className="notask-title">
+              Currently there are no Pending tasks for you.
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <div className="app-title">
+          <h1>Reference articles as per your todo List :</h1>
         </div>
-      ) : (
-        <div className="notask-div">
-          <p className="notask-title">
-            Currently there are no Pending tasks for you.
-          </p>
-        </div>
-      )}
+
+        {allList.length > 0 ? (
+          <div></div>
+        ) : (
+          <div className="notask-div">
+            <p className="notask-title">
+              Create a task to display the reference articles.
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
